44ed8c8f8f647da64aaa33503b92c6412c8932ff
deciding <zhangzn710@gmail.com>
2019-03-06 07:07:56 +0000

    minor faster

--- inference.py
+++ inference.py
@@ -101,21 +101,21 @@ def get_dvector_of_dir(sess, feeder, model, args):
             continue
         wav1_out = sess.run(model.norm_out, feed_dict={model.input_batch:wav1_data})
         new_total_vectors=np.concatenate((total_vectors, wav1_out), axis=0) if total_vectors is not None else wav1_out
-        if total_vectors is not None:
-            #dvec1=np.mean(new_total_vectors, axis=0)
-            #dvec2=np.mean(total_vectors,axis=0)
-            dvec1=np.mean(total_vectors, axis=0)
-            dvec2=np.mean(wav1_out,axis=0)
-            cossim=dot(dvec1, dvec2)/(norm(dvec1)*norm(dvec2))
-            if cossim < 0.5:
-                small_sim_cnt+=1
-                print("=================================================================================================================" + str(rmse(dvec1,dvec2)) + "   " + str(cossim) + "   " + str(np.size(new_total_vectors, 0)) + "   " + str(small_sim_cnt))
-            #if cossim > 0.999:
-            #    np.save('a.npy', dvec1)
-            #    return
+        #if total_vectors is not None:
+        #    #dvec1=np.mean(new_total_vectors, axis=0)
+        #    #dvec2=np.mean(total_vectors,axis=0)
+        #    dvec1=np.mean(total_vectors, axis=0)
+        #    dvec2=np.mean(wav1_out,axis=0)
+        #    cossim=dot(dvec1, dvec2)/(norm(dvec1)*norm(dvec2))
+        #    if cossim < 0.5:
+        #        small_sim_cnt+=1
+        #        print("=================================================================================================================" + str(rmse(dvec1,dvec2)) + "   " + str(cossim) + "   " + str(np.size(new_total_vectors, 0)) + "   " + str(small_sim_cnt))
+        #    #if cossim > 0.999:
+        #    #    np.save('a.npy', dvec1)
+        #    #    return
 
-            #print(np.mean(total_vectors,axis=0))
-            #print(np.mean(new_total_vectors,axis=0))
+        #    #print(np.mean(total_vectors,axis=0))
+        #    #print(np.mean(new_total_vectors,axis=0))
 
         total_vectors=new_total_vectors
     wav1_dvector = np.mean(total_vectors, axis=0)
